4995b239d6359df64756e8c954ce308f9a8adaa7
MarcinDl <marcindl@hotmail.com>
2021-10-08 10:57:18 +0200

    backend

--- backend/ical/ical-logic.js
+++ backend/ical/ical-logic.js
@@ -1,8 +1,15 @@
 const ical = require('node-ical');
 
+const room106 = "http://www.cdism.sum.edu.pl/ics/calendar-106.ics"
+
+const room = {
+    firstFloor: [106,107,108,111,112,113,114,117],
+    secondFlooor: [201,204,206,208,210,211,213,219,220,221,222,223]
+}
 function allEvents() {
+
     return new Promise((resolve, reject) => {
-        ical.fromURL('http://www.cdism.sum.edu.pl/ics/calendar-106.ics', {}, function (err, dataFromICS) {
+        ical.fromURL(room106, {}, function (err, dataFromICS) {
             let today = new Date();
             const dd = String(today.getDate()).padStart(2, '0');
             const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -12,7 +19,7 @@ function allEvents() {
             let lecturesInfo = [];
 
             for(let singleData in dataFromICS){
-                // console.log(dataFromICS[singleData])
+                console.log(dataFromICS[singleData])
                 if (today == dataFromICS[singleData].end.toLocaleDateString()){
                     let singleLectureInfo = {
                         summary: dataFromICS[singleData].summary,
@@ -20,6 +27,7 @@ function allEvents() {
                         endDate: dataFromICS[singleData].end.toLocaleDateString(),
                         startTime: dataFromICS[singleData].start.getHours() + ":" + dataFromICS[singleData].start.getMinutes(),
                         endTime: dataFromICS[singleData].end.getHours() + ":" + dataFromICS[singleData].end.getMinutes(),
+                        room: room106.substr(-7, 3),
                         openingHours: dataFromICS[singleData].start.getHours(),
                         openingMinutes: dataFromICS[singleData].start.getMinutes(),
                         closingHours: dataFromICS[singleData].end.getHours(),
