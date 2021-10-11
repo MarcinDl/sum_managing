08b43680878ee341213e5a710abaa4904cf6c6cc
MarcinDl <marcindl@hotmail.com>
2021-10-11 14:36:32 +0200

    backend

--- backend/ical/ical-logic.js
+++ backend/ical/ical-logic.js
@@ -1,19 +1,16 @@
 const ical = require('node-ical');
 
-const room106 = "http://www.cdism.sum.edu.pl/ics/calendar-106.ics"
-const room107 = "http://www.cdism.sum.edu.pl/ics/calendar-107.ics"
-const room108 = "http://www.cdism.sum.edu.pl/ics/calendar-108.ics"
+// const room106 = "http://www.cdism.sum.edu.pl/ics/calendar-106.ics"
+// const room107 = "http://www.cdism.sum.edu.pl/ics/calendar-107.ics"
+// const room108 = "http://www.cdism.sum.edu.pl/ics/calendar-108.ics"
 
 const icsLink = "http://www.cdism.sum.edu.pl/ics/calendar-";
 const icsExension = ".ics";
 
-const room = {
+const cdism = {
     firstFloor: [106,107,108,111,112,113,114,117],
     secondFloor: [201,204,206,208,210,211,213,219,220,221,222,223]
 }
-
-let firstFloorArray = [];
-let secondFloorArray = [];
 
 function allEvents() {
 
@@ -23,83 +20,34 @@ function allEvents() {
     const yyyy = today.getFullYear();
     today = dd + '/' + mm + '/' + yyyy;
 
-    let neww =  new Promise((resolve, reject) => {
+    let allEventsInCDiSM = []
+    for (let roomNumber of [...cdism.firstFloor,...cdism.secondFloor]){
+        let singleRoom =  new Promise((resolve, reject) => {
 
-        ical.fromURL(room106, {}, function (err, dataFromICS) {
-            let lecturesInfo = [];
+            ical.fromURL(`${icsLink}${roomNumber}${icsExension}`, {}, function (err, dataFromICS) {
+                let lecturesInfo = [];
+    
+                for(let singleData in dataFromICS){
+                    if (today == dataFromICS[singleData].end.toLocaleDateString()){
+                        let singleLectureInfo = {
+                            summary: dataFromICS[singleData].summary,
+                            date: dataFromICS[singleData].start.toLocaleDateString(),
+                            startTime: dataFromICS[singleData].start.getHours() + ":" + dataFromICS[singleData].start.getMinutes(),
+                            endTime: dataFromICS[singleData].end.getHours() + ":" + dataFromICS[singleData].end.getMinutes(),
+                            room: roomNumber,
+                        }
+                        lecturesInfo.push(singleLectureInfo)
+                    } 
+                }
+    
+                resolve(lecturesInfo)
+            });
+        })
 
-            for(let singleData in dataFromICS){
-                // console.log(dataFromICS[singleData])
-                if (today == dataFromICS[singleData].end.toLocaleDateString()){
-                    let singleLectureInfo = {
-                        summary: dataFromICS[singleData].summary,
-                        date: dataFromICS[singleData].start.toLocaleDateString(),
-                        // startDate: dataFromICS[singleData].start.toLocaleDateString(),
-                        // endDate: dataFromICS[singleData].end.toLocaleDateString(),
-                        startTime: dataFromICS[singleData].start.getHours() + ":" + dataFromICS[singleData].start.getMinutes(),
-                        endTime: dataFromICS[singleData].end.getHours() + ":" + dataFromICS[singleData].end.getMinutes(),
-                        room: room106.substr(-7, 3),
-                    }
-                    lecturesInfo.push(singleLectureInfo)
-                    // console.log(lecturesInfo)
-                } 
-            }
+        allEventsInCDiSM.push(singleRoom)
+    }
 
-            resolve(lecturesInfo)
-        });
-    })
-    let neww2 =  new Promise((resolve, reject) => {
-
-        ical.fromURL(room107, {}, function (err, dataFromICS) {
-            let lecturesInfo = [];
-
-            for(let singleData in dataFromICS){
-                // console.log(dataFromICS[singleData])
-                if (today == dataFromICS[singleData].end.toLocaleDateString()){
-                    let singleLectureInfo = {
-                        summary: dataFromICS[singleData].summary,
-                        date: dataFromICS[singleData].start.toLocaleDateString(),
-                        // startDate: dataFromICS[singleData].start.toLocaleDateString(),
-                        // endDate: dataFromICS[singleData].end.toLocaleDateString(),
-                        startTime: dataFromICS[singleData].start.getHours() + ":" + dataFromICS[singleData].start.getMinutes(),
-                        endTime: dataFromICS[singleData].end.getHours() + ":" + dataFromICS[singleData].end.getMinutes(),
-                        room: room107.substr(-7, 3),
-                    }
-                    lecturesInfo.push(singleLectureInfo)
-                    // console.log(lecturesInfo)
-                } 
-            }
-
-            resolve(lecturesInfo)
-        });
-    })
-    let neww3 =  new Promise((resolve, reject) => {
-
-        ical.fromURL(room108, {}, function (err, dataFromICS) {
-            let lecturesInfo = [];
-
-            for(let singleData in dataFromICS){
-                // console.log(dataFromICS[singleData])
-                if (today == dataFromICS[singleData].end.toLocaleDateString()){
-                    let singleLectureInfo = {
-                        summary: dataFromICS[singleData].summary,
-                        date: dataFromICS[singleData].start.toLocaleDateString(),
-                        // startDate: dataFromICS[singleData].start.toLocaleDateString(),
-                        // endDate: dataFromICS[singleData].end.toLocaleDateString(),
-                        startTime: dataFromICS[singleData].start.getHours() + ":" + dataFromICS[singleData].start.getMinutes(),
-                        endTime: dataFromICS[singleData].end.getHours() + ":" + dataFromICS[singleData].end.getMinutes(),
-                        room: room108.substr(-7, 3),
-                    }
-                    lecturesInfo.push(singleLectureInfo)
-                    // console.log(lecturesInfo)
-                } 
-            }
-
-            resolve(lecturesInfo)
-        });
-    })
-
-    return Promise.all([neww, neww2, neww3])
+    return Promise.all(allEventsInCDiSM)
 }
 
 module.exports = {
