15afd9c0a0761ca10bfbcdabff1b4aef439a8462
MarcinDl <marcindl@hotmail.com>
2021-10-12 13:15:03 +0200

    frontend cupertino pane

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, toArray } from 'rxjs/operators';
+import { map,  } from 'rxjs/operators';
+
+
+import { CupertinoPane, CupertinoSettings } from "cupertino-pane";
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -27,11 +31,28 @@ export class HomePage {
   ngOnInit(){
     this.lecturesSummary$ = this.http.get<any>("http://localhost:3000/api").pipe(map( result => result.sort(this.sortByStartime)))
     console.log("this.lecuresSummary$",this.lecturesSummary$)
+
+    this.openPane()
   }
 
   sortByStartime = (a, b) => {
     const nameA = a.startTime;
     const nameB = b.startTime;
     return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0;
+  }
+
+  openPane() {
+    const myPane = new CupertinoPane(
+      ".cupertino-pane", // Pane container selector
+      {
+        parentElement: "ion-content", // Parent container
+        breaks: {
+          middle: { enabled: true, height: 100 },
+          bottom: { enabled: true, height: 20 }
+        },
+        bottomClose: true
+      }
+    );
+    myPane.present({ animate: true });
   }
 }
