134e3b833767f50fa8ca69dbb94a0c606f8107cc
MarcinDl <marcindl@hotmail.com>
2021-10-12 11:07:01 +0200

    frontend display current lectures

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map, toArray } from 'rxjs/operators';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,9 +11,18 @@ export class HomePage {
 
   lecturesSummary = [];
   lecturesSummary$ :Observable<any[]>;
+  actualTime: any;
   constructor(
     private http:HttpClient
-  ) {}
+  ) {
+    let today = new Date();
+
+    let mm = String(today.getHours()).padStart(2, '0'); //January is 0!
+    let ss = String(today.getMinutes()).padStart(2, '0'); //January is 0!
+    console.log(mm+":"+ss)
+    this.actualTime = mm+":"+ss
+  }
+
 
   ngOnInit(){
     this.lecturesSummary$ = this.http.get<any>("http://localhost:3000/api")
